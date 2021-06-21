58df7100dfb14610815ab58cbaf0ba55833c279a
gayoungkang <gayoung425@gmail.com>
2021-06-21 20:22:59 +0900

    모바일 웹 세로 테스트
    스크립트 수정
    
    테스트
    
    스크립트 수정
    
    테스트중
    
    수정

--- js/common.js
+++ js/common.js
@@ -22,3 +22,29 @@ menu.addEventListener('click', function() {
 close.addEventListener('click', function () {
     nav.classList.remove('active')
 });
+// 모바일 화면단 강제로 가로모드로 변환하기
+const filter = "win16|win32|win64|mac|macintel"; 
+const html = document.querySelector('html');
+  if ( navigator.platform ) { 
+    if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
+      //mobile 
+      alert('mobile 접속!!!!!!!'); 
+      window.addEventListener("orientationchange", function() {
+        console.log(window.orientation)
+        if(window.orientation != undefined) {
+            if(window.orientation == -90) {
+                html.style.transform = "rotate(90deg)";
+                html.style.overflow = "hidden";
+                html.style.position = "absolute";
+                html.style.top = "0";
+                html.style.left = "0";
+                html.style.width = "100%";
+                html.style.height = "100%"
+            }
+        }
+    });
+    } else { 
+      //pc 
+      alert('pc 접속'); 
+  } 
+}
